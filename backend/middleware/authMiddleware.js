cdec5e27391d4db45d5c153e4c0bf31af9e345cb
deepakjain06 <deepakjain0621@gmail.com>
2025-07-12 10:18:41 +0530

    bug resolved in addfood api

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
     const token = authHeader.split(' ')[1];
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = { id: decoded.userId }; // ✅ set as `req.user.id` to match your usage
+        req.userId = decoded.userId; // ✅ This line is VERY IMPORTANT // ✅ set as `req.user.id` to match your usage
         next();
     } catch (err) {
         res.status(403).json({ message: 'Invalid token' });
